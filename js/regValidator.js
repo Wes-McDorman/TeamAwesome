680135285a834290fa68cebb92f03cebd95921d0
Jp <jsprouse@ggc.edu>
2016-09-22 17:02:40 -0400

    stuRegister.html update
    added validation js

---
+++ js/regValidator.js
@@ -0,0 +1,101 @@
+// validator.js
+//   An example of input validation using the change and submit 
+
+
+
+//   events
+// The event handler function for the name text box
+function chkName() {
+  var myName = document.getElementById("fName");
+// Test the format of the input name 
+//   Allow the spaces after the commas to be optional
+//   Allow the period after the initial to be optional
+  var pos = myName.value.search(/^[A-Z][a-z]+, ?[A-Z][a-z]+, ?[A-Z]\.?$/);
+  var pos2 = myName.value.search(/^[A-Z][a-z]+, ?[A-Z][a-z]+$/);
+
+if (((pos === 0) || (pos2 === 0))) {
+    document.getElementById("errorSpace").innerHTML = "";
+    return true; 
+  } else
+      document.getElementById("errorSpace").innerHTML += "The name you entered (" + myName.value +
+          ") is not in the correct form." +
+          "The correct form is:" +
+          " Last-name, First-name, Middle-initial. ";
+    return false;
+}
+
+
+
+
+// The event handler function for the phone number text box
+function chkPhone() {
+  var myPhone = document.getElementById("phone");
+// Test the format of the input phone number
+  var pos = myPhone.value.search(/^\d{3}-\d{3}-\d{4}$/);
+  if (pos != 0) {
+      document.getElementById("errorSpace").innerHTML += "<br>The phone number you entered (" + myPhone.value +
+          ") is not in the correct format." +
+          "The correct form is: ddd-ddd-dddd. ";
+    return false;
+  } else
+    return true;
+}
+
+
+// The event handler function for the email text box
+function chkEmail() {
+  var myEmail = document.getElementById("inputEmail");
+// Test the format of the input phone number
+  var emailRegEx =  /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  if (!(myEmail.value.match(emailRegEx))) {
+      document.getElementById("errorSpace").innerHTML += "<br>The email you entered (" + myEmail.value +
+          ") is not in the correct form. \n" +
+          "The correct form is: xxx@xxx.xxx \n";
+    
+    return false;
+  } else
+    return true;
+}
+
+function chkEmailMatch() {
+  var myEmail = document.getElementById("inputEmail");
+  var myEmail2 = document.getElementById("inputEmail2");
+  if (!(myEmail2.value.match(myEmail.value))) {
+      document.getElementById("errorSpace").innerHTML += "<br>The email you entered (" + myEmail2.value +
+          ") does not match  (" + myEmail.value +
+          ")\n";
+    document.getElementById("inputEmail2").focus();
+    return false;
+  } else
+     
+    return true;
+}
+
+function chkPasswordMatch() {
+  var myPass = document.getElementById("inputPassword");
+  var myPass2 = document.getElementById("inputPassword2");
+  if (!(myPass2.value.match(myPass.value))) {
+      document.getElementById("errorSpace").innerHTML += "<br>Please check that your passwords match and try again.\n";
+    document.getElementById("inputEmail2").focus();
+    return false;
+  } else
+    return true;
+}
+
+
+
+/*function formValidation(){
+    var submitValid = false;
+    if (chkName){
+        if(chkEmail){
+            if(chkEmailMatch){
+                if(chkPhone){
+                    if(chkPasswordMatch){
+                        submitValid = true;
+                    }
+                }
+            }
+        }
+    }
+   return submitValid; 
+}*/
---
+++ js/regValidatorr.js
@@ -0,0 +1,9 @@
+// validatorr.js
+//   Register the event handlers for validator.html
+
+document.getElementById("fName").onchange = chkName;
+document.getElementById("phone").onchange = chkPhone;
+document.getElementById("inputEmail").onchange = chkEmail;
+document.getElementById("inputEmail2").onchange = chkEmailMatch;
+//document.getElementById("inputPassword").onchange = chkName;
+document.getElementById("inputPassword2").onchange = chkPasswordMatch;
