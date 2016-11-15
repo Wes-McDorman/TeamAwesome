90f3b1eb53ada6d973f61bc24df0e418df4dae2b
Wes-McDorman <wmcdorma@ggc.edu>
2016-11-15 18:25:58 -0500

    Deleted stu_session, fixed incorrect name on adminstuview

--- stu_session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if ((isset($_SESSION['login']) && $_SESSION['login'] == '1')) {
-        
-        include("connection.php");
-        include("stuPage.html");
-    }
-?>
