9a92ce1bdffc5e9cd97962cf740b7a0b471a9774
Wes McDorman <wmcdorma@ggc.edu>
2016-11-15 22:27:07 -0500

    Student and Volunteer list now Empty and Refill on show more button press

--- js/showMore_ButtonLogic.js
+++ js/showMore_ButtonLogic.js
@@ -1,14 +1,46 @@
 
 
-var showButton = document.getElementById('showMoreButton');
+var showVolButton = document.getElementById('showVolButton');
 var volList = document.getElementById('volList');
+
+var showStuButton = document.getElementById('showStuButton');
+var StuList = document.getElementById('StuList');
 
 var userID = 10001;
 
-showButton.onclick = function clickIt() {
+
+//Volunteer show more button logic
+if (showVolButton != null) {
+showVolButton.onclick = function clickVol() {
+  while (volList.hasChildNodes()) {
+    volList.removeChild(volList.firstChild);
+  }
   for(i = 0; i < 10; i++) {
     var entry = document.createElement('li');
     entry.appendChild(document.createTextNode(userID));
     volList.appendChild(entry);
   }
+  userID++; //for testing delete after
+}
+
+
+
+}
+
+
+//Student show more button logic
+if (showStuButton != null) {
+showStuButton.onclick = function clickStu() {
+  while (stuList.hasChildNodes()) {
+    stuList.removeChild(stuList.firstChild);
+  }
+  for(i = 0; i < 10; i++) {
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(userID));
+    stuList.appendChild(entry);
+  }
+  userID++; //for testing delete after
+}
+
+
 }
