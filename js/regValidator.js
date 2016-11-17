801cdc9434f0340e0e6daa1be8d371e4fe7145bf
Jp <jsprouse@ggc.edu>
2016-11-16 21:55:48 -0500

    Fixed SOME of the validation issues.

--- js/regValidator.js
+++ js/regValidator.js
@@ -14,9 +14,9 @@ if (pos === 0) {
     document.getElementById("errorFName").innerHTML = "";
     return true; 
   } else
-      document.getElementById("errorFName").innerHTML = "The name you entered (" + myName.value +
-          ") is not in the correct form." +
-          "There should only be letters.";
+      document.getElementById("errorFName").innerHTML = myName.value +
+ 
+          " - Warning: Letters only.";
     return false;
 }
 
@@ -30,9 +30,8 @@ if (pos === 0) {
     document.getElementById("errorLName").innerHTML = "";
     return true; 
   } else
-      document.getElementById("errorLName").innerHTML = "The name you entered (" + myName.value +
-          ") is not in the correct form." +
-          "There should only be letters.";
+      document.getElementById("errorLName").innerHTML =  myName.value +
+            " - Warning: Letters only.";
     return false;
 }
 
@@ -41,13 +40,13 @@ if (pos === 0) {
 
 // The event handler function for the phone number text box
 function chkPhone() {
-  var myPhone = document.getElementById("phone");
+  var myPhone = document.getElementById("inputPhone");
 // Test the format of the input phone number
   var pos = myPhone.value.search(/^\d{3}-\d{3}-\d{4}$/);
   if (pos != 0) {
       document.getElementById("errorPhone").innerHTML = "The phone number you entered (" + myPhone.value +
           ") is not in the correct format." +
-          "The correct form is: ddd-ddd-dddd. ";
+          "The expected format is: ddd-ddd-dddd. ";
     return false;
   } else
       document.getElementById("errorPhone").innerHTML = "";
@@ -61,8 +60,8 @@ function chkEmail() {
 // Test the format of the input phone number
   var emailRegEx =  /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
   if (!(myEmail.value.match(emailRegEx))) {
-      document.getElementById("errorEmail").innerHTML = "The email you entered (" + myEmail.value +
-          ") is not in the correct form. \n" +
+      document.getElementById("errorEmail").innerHTML = myEmail.value +
+          " - Error:\n" +
           "The correct form is: xxx@xxx.xxx \n";
     
     return false;
@@ -89,7 +88,7 @@ function chkPasswordMatch() {
   var myPass = document.getElementById("password");
   var myPass2 = document.getElementById("password2");
   if (!(myPass2.value.match(myPass.value))) {
-      document.getElementById("errorPassMatch").innerHTML = "Please check that your passwords match and try again.\n";
+      document.getElementById("errorPassMatch").innerHTML = "Error: Password does not match.\n";
     document.getElementById("password").focus();
     return false;
   } else
@@ -102,18 +101,32 @@ function chkContactPhone() {
 // Test the format of the input phone number
   var pos = myPhone.value.search(/^\d{3}-\d{3}-\d{4}$/);
   if (pos != 0) {
-      document.getElementById("errorContactPhone").innerHTML = "The phone number you entered (" + myPhone.value +
-          ") is not in the correct format." +
-          "The correct form is: ddd-ddd-dddd. ";
+      document.getElementById("errorContactPhone").innerHTML = myPhone.value +
+
+          " - The expected form is: ddd-ddd-dddd. ";
     return false;
   } else
       document.getElementById("errorContactPhone").innerHTML = "";
     return true;
 }
 
+function chkZip() {
+    var zip = document.getElementById("zip");
+    var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zip.value);
+    
+      if (isValidZip != 0) {
+      document.getElementById("errorZip").innerHTML =  zip.value +" is not in the correct format." ;
+    return false;
+  } else
+      document.getElementById("errorZip").innerHTML = "";
+    return true;
+}
+
+
+
 /*function formValidation(){
     var submitValid = false;
-    if (chkName){
+    if (chkFName){
         if(chkEmail){
             if(chkEmailMatch){
                 if(chkPhone){
--- js/regValidatorr.js
+++ js/regValidatorr.js
@@ -2,11 +2,12 @@
 // validatorr.js
 //   Register the event handlers for validator.html
 
-document.getElementById("fName").onchange = chkName;
-document.getElementById("lName").onchange = chkName;
-document.getElementById("phone").onchange = chkPhone;
-document.getElementById("inputEmail").onchange = chkEmail;
-document.getElementById("inputEmail2").onchange = chkEmailMatch;
-//document.getElementById("inputPassword").onchange = chkName;
-document.getElementById("inputPassword2").onchange = chkPasswordMatch;
+document.getElementById("fName").onchange = chkFName;
+document.getElementById("lName").onchange = chkLName;
+document.getElementById("inputPhone").onchange = chkPhone;
+document.getElementById("email").onchange = chkEmail;
+document.getElementById("email2").onchange = chkEmailMatch;
+
+document.getElementById("password2").onchange = chkPasswordMatch;
 document.getElementById("contactPhone").onchange = chkContactPhone;
+document.getElementById("zip").onchange = chkZip;
